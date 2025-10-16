659f711b8a6101a464d64de776b14e0cd92760f4
Fateme Keshvari <mochis850uto@gmail.com>
2025-10-16 18:30:42 +0330

    commit oop_cw9

--- week6/oop_cw/cw9.py
+++ week6/oop_cw/cw9.py
@@ -1,18 +1,15 @@
 class User:
-    def __init__(self, user_name: str, email: str, privilegs: list, comments: list):
+    def __init__(self, user_name: str, email: str):
         self.user_name = user_name
         self.email = email
-        self.privilegs = privilegs
-        self.comments = comments
-    
     def show_info(self):
         print(f"User : {self.user_name}\nEmail : {self.email}")
     def login(self):
         print(f"{self.user_name} has logged in")
 
 class AdminUser(User):
-    def __init__(self, user_name, email, privilegs, comments):
-        super().__init__(user_name , email, privilegs, comments)
+    def __init__(self, user_name, email, privilegs):
+        super().__init__(user_name, email)
         self.privilegs = privilegs
     def ban_user(self, user):
         print(f"Admin {self.user_name} has banned user {user}.")
@@ -21,24 +18,24 @@ class AdminUser(User):
         print(self.privilegs)
 
 class  NormalUser(User):
-    def __init__(self,user_name, email, privilegs, comments):
-        super().__init__(user_name, email, privilegs, comments)
+    def __init__(self,user_name, email, comments):
+        super().__init__(user_name, email)
         self.comments = comments
 
     def post_comment(self):
-        # for txt in self.comments:
+        
         txt = input("please leave a comment: ")
         self.comments.append(txt)
         print(f"{self.user_name} posted a new comment :\n{txt}")
     def show_comments(self):
         print(self.comments)
 
-user = User("john", "smartjo12@gmail.com", [5, 3, 10, 2], ["well done", "good job man", "yes you got it"])
-admin_User = AdminUser("noa", "notanother2@gmail.com", [5, 4, 8, 2], ["good", "nice"])
-normal_user = NormalUser("johanson", "smjijio12@gmail.com", [2,5], ["very good", "good job", "yes"])
+user = User("john", "smartjo12@gmail.com") 
+admin_User = AdminUser("noa", "notanother2@gmail.com", [5, 4, 8, 2])  
+normal_user = NormalUser("johanson", "smjijio12@gmail.com", ["very good", "good job", "yes"])  
 user.show_info()
 user.login()
-admin_User.ban_user("johnii")
+admin_User.ban_user("piter")
 admin_User.show_privilegs()
 normal_user.post_comment()
 normal_user.show_comments()
