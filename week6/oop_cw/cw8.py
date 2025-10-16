cb8023fdaeb458814bec8f09733b429055ea1aa8
Fateme Keshvari <mochis850uto@gmail.com>
2025-10-16 20:46:54 +0330

    third commit for oop_cw8

--- week6/oop_cw/cw8.py
+++ week6/oop_cw/cw8.py
@@ -4,11 +4,10 @@ class Employee:
         self.base_salary = base_salary
 
     def calculate_salary(self):
-        print(self.base_salary)
+        return self.base_salary
     
     def info(self):
         print(f"Employee: {self.name}, Base salary: {self.base_salary}")
-            
 
 class FullTimeEmployee(Employee):
     def __init__(self, name, base_salary, bonus: int):
@@ -17,9 +16,8 @@ class FullTimeEmployee(Employee):
     
     def calculate_bonus(self):
         general_salary = self.base_salary + self.bonus
-        return f"{self.base_salary} + {self.bonus} = {general_salary}"
-        # print(f"Total salary with bonus for {self.name} is {general_salary}")
-
+        return general_salary
+    
 class PartTimeEmployee(Employee):
     def __init__(self, name, base_salary, hourly_rate, hours_worked):
         super().__init__(name, base_salary)
@@ -27,13 +25,12 @@ class PartTimeEmployee(Employee):
         self.hours_worked = hours_worked
     def calculate_hour(self):
         result = self.hourly_rate * self.hours_worked
-        return f"{self.hourly_rate} * {self.hours_worked} = {result}"
-        # print(f"the daily salary for {self.name} is {result}")
+        return result
 
 employee = Employee("John", 6000)
 full_time = FullTimeEmployee("Peter", 6000, 586)
 part_time = PartTimeEmployee("Walter", 3000, 12.5, 8)
-employee.calculate_salary()
+print(employee.calculate_salary())
 employee.info()
-print(f"Total salary with bonus :", full_time.calculate_bonus())
-print(f"daily salary :", part_time.calculate_hour())
+print(f"{full_time.name} Total salary with bonus : {full_time.calculate_bonus()}")
+print(f"{part_time.name} daily salary : {part_time.calculate_hour()}")
